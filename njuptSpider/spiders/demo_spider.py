76d82211804e24ad83c2bcd8d975193de70c37d5
Hi-Light <glgl.makeit@gmail.com>
2016-05-11 18:59:13 +0800

    修复混用tab和space的错误

--- njuptSpider/spiders/demo_spider.py
+++ njuptSpider/spiders/demo_spider.py
@@ -8,5 +8,5 @@ class DemoSpider(Spider):
 	]
 	def parse(self,response):
 		filename = response.url.split("/")[-2]
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+		with open(filename, 'wb') as f:
+			f.write(response.body)
