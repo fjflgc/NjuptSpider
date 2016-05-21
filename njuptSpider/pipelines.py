76601877b5684ec2f371d43d58bc058074fd2599
Hi-Light <glgl.makeit@gmail.com>
2016-05-21 16:27:23 +0800

    实现利用数据库保存爬取过的url

--- njuptSpider/pipelines.py
+++ njuptSpider/pipelines.py
@@ -6,10 +6,7 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import pymysql
 from scrapy import signals
-
-# 经过管道将符合条件的item保存到数据库和json文件中，滤掉不符合条件的item
-from scrapy.exporters import JsonItemExporter
-
+# 经过管道将符合条件的item保存到数据库中，滤掉不符合条件的item
 
 class Pipeline(object):
     def __init__(self):
@@ -25,16 +22,8 @@ class Pipeline(object):
     def spider_opened(self, spider):
         self.conn = pymysql.connect(host='localhost', user='root', passwd='mysqlGL0111', db='njuptInfo', charset='utf8')
         self.cur = self.conn.cursor()
-        file = open('./result.json', 'w+b')
-        self.files[spider] = file
-        self.exporter = JsonItemExporter(file, ensure_ascii=False)  # 添加ensure_ascii=False用于使json保存中文不乱码
-        self.exporter.start_exporting()
-        self.count = 0
 
     def spider_closed(self, spider):
-        self.exporter.finish_exporting()
-        file = self.files.pop(spider)
-        file.close()
         self.cur.close()
         self.conn.close()
 
@@ -42,13 +31,9 @@ class Pipeline(object):
         str = 'insert into showNews_test (date,url,content,title,count) values '
         str += "('%s','%s','%s','%s','%s');\r\n" % (
             item['date'], item['url'], item['content'], item['title'], item['count'])
-        print(item['title'])
         try:
             ss = self.cur.execute(str)
-            self.count = self.count + 1
-            print(self.count)
         except pymysql.err.IntegrityError as e:
-            print("数据库中已存在")
+            print("数据库中已存在的文章")
         self.conn.commit()  # 各种教程都没提到提交这一步啊喂!!!!!!
-        self.exporter.export_item(item)
         return item
--- njuptSpider/spiders/njupt_spider.py
+++ njuptSpider/spiders/njupt_spider.py
@@ -1,20 +1,20 @@
 from scrapy import Request
 from scrapy.spiders import BaseSpider
 from ..items import Item, ItemTest
+import pymysql
 
-
-urls = set([])
+# 链接为文件的后缀名
 trash = (
     'doc', 'docx', 'xls', 'xlsx', 'zip', 'rar', 'pdf', 'ppt', 'pptx', 'pptm', 'mp3', 'jpg', 'gif', 'mp4', 'png', 'x',
     'rar')
+
 import re
 
-r1 = re.compile('[a-zA-z]+://[^\s]*info[1-9]*.htm$')  # 正则表达式匹配文章url
+r1 = re.compile('[a-zA-z]+://[^\s]*info[0-9]*.htm$')  # 正则表达式匹配文章url
 r2 = re.compile('href="/[^\s]*"')
 r3 = re.compile('src="/[^\s]*"')
 
 
-# 实现爬南邮首页所有链接的标题和url并保存为json
 class NjuptSpider(BaseSpider):
     name = "njupt"
     allowed_domains = ["jwc.njupt.edu.cn"]
@@ -22,27 +22,37 @@ class NjuptSpider(BaseSpider):
         "http://jwc.njupt.edu.cn/",
     ]
 
+    def __init__(self):
+        self.conn = pymysql.connect(host='localhost', user='root', passwd='mysqlGL0111', db='urls', charset='utf8')
+        self.cur = self.conn.cursor()
+
     def parse(self, response):
         for link in response.xpath('//a'):
-            # item = Item()
             if link.xpath('@href').extract():  # 判断a标签是否有href属性
                 url = link.xpath('@href').extract()[0]
+                if ('mailto' in url) or ('javascript:' in url):  # 不爬取和保存邮箱链接,javascript链接
+                    continue
                 if 'http://' not in url:  # 将相对链接转换为绝对链接
                     url = 'http://' + response.url.split('/')[2] + url
-                if (url in urls) or ('mailto' in url) or ('javascript:' in url):  # 不爬取和保存邮箱链接,javascript链接
+                str = 'insert into craweled_urls (url) values '
+                str += "('%s');\r\n" % (url)
+                try:
+                    ss = self.cur.execute(str)
+                except pymysql.err.IntegrityError as e:
+                    print("已爬取过的url")
                     continue
+                self.conn.commit()
+                if r1.match(url):
+                    yield Request(url, callback=self.parse_article)
+                elif (not url.lower().endswith(trash)) and (not url.startswith('http://acm')):  # 不爬取noj和文件链接
+                    yield Request(url, callback=self.parse)  # 如果url合法，对该url继续爬
                 else:
-                    urls.add(url)
-                    if r1.match(url):
-                        yield Request(url, callback=self.parse_article)
-                    elif (not url.lower().endswith(trash)) and (not url.startswith('http://acm')):  # 不爬取noj和文件链接
-                        yield Request(url, callback=self.parse)  # 如果url合法，对该url继续爬
-                    else:
-                        continue
+                    continue
 
     # 实现对文章页面的处理
     def parse_article(self, response):
         item = ItemTest()
+        # 此处的xpath值需要根据不同的部门进行设置
         item['date'] = response.xpath("//span[@class='STYLE2']/text()").extract()[0][5:15]
         content = response.xpath('//*[@id="container_content"]/table/tr/td/table[5]').extract()[0].replace("'", '"')
         # 通过正则表达式转换相对链接为绝对链接
